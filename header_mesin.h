57571231e92cccb21c05f049271eefe0cdf2078f
fairuzasf <fayyyus97@gmail.com>
2026-06-06 21:30:14 +0700

    Update header

--- header_mesin.h
+++ header_mesin.h
@@ -12,8 +12,14 @@ typedef struct { 	//Pake typedef agar di main tinggal panggil namanya saja ex: H
     int skor;
 } Hadiah;
 
+typedef struct {
+int x;
+int y;
+} Gerak;
+
 void START(FILE *arsip);  	//START buat nnti program mulai baca karakter awal 
 void ADV(FILE *arsip);	  	//ADV buat maju satu karakter ke depan jika udah baca file sebelumnya
+void wait(float x);		// deklarasi fungsi wait 
           
 int baca_angka(FILE *arsip);	//ini buat baca dr file karakter perkarakter angka jadi satu. ex: inputan buat x itu 1,0 dijadiin 10
 void baca_string(FILE *arsip, char *s); //klo ini buat baca dr file karakter huruf ex: a,a jadi aa
