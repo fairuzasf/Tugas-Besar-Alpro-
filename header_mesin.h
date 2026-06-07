9bd2760618c603d5b8194aaca1e2334af3d6ba98
fairuzasf <fayyyus97@gmail.com>
2026-06-07 11:48:16 +0700

    mendeklarasikan 1 fungsi dan menghapus 1 library

--- header_mesin.h
+++ header_mesin.h
@@ -2,7 +2,6 @@
 #define header_mesin_h
 
 #include <stdio.h>
-#include <stdbool.h>
 #include <time.h>
 
 typedef struct { 	//Pake typedef agar di main tinggal panggil namanya saja ex: Hadiah, ini biar tipe data beda tp bisa jd satu yaa
@@ -20,6 +19,7 @@ int y;
 void START(FILE *arsip);  	//START buat nnti program mulai baca karakter awal 
 void ADV(FILE *arsip);	  	//ADV buat maju satu karakter ke depan jika udah baca file sebelumnya
 void wait(float x);		// deklarasi fungsi wait 
+void AbaikanBlank(FILE *arsip);
           
 int baca_angka(FILE *arsip);	//ini buat baca dr file karakter perkarakter angka jadi satu. ex: inputan buat x itu 1,0 dijadiin 10
 void baca_string(FILE *arsip, char *s); //klo ini buat baca dr file karakter huruf ex: a,a jadi aa
