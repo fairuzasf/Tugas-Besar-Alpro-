cf8ebe6e427c98582f0f3eb5f072aa51ef3b998a
Attar124 <2510514001@mahasiswa.upnvj.ac.id>
2026-06-08 20:51:33 +0700

    Update fungsi_mesin.c

--- fungsi_mesin.c
+++ fungsi_mesin.c
@@ -137,3 +137,17 @@ void tulisGerak(Gerak arrGerak[], int *jumlahGerak, int x, int y) {
         printf("Gagal membuka file tgerak.txt untuk ditulis!\n");
     }
 }
+
+void cetakTabelHadiah(Hadiah arrHadiah[], int jumlahHadiah) { 
+    printf("Isi hadiah saat ini\n"); 
+    printf("------------------------------------------\n"); 
+    printf("|%-4s|%-4s|%-6s|%-6s|\n","x","y", "nama", "skor"); //buat header tabell
+        for (int i = 0; i< jumlahHadiah; i++) { //loop buatt nyetak tiap baris data hadiah di tabel
+            printf("|%-4d|%-4d|%-6s|%-6d|\n", 
+        arrHadiah[i].x, 
+        arrHadiah[i].y, 
+        arrHadiah[i].nama, 
+        arrHadiah[i].skor); 
+} 
+    printf("--------------------------------------------------------\n"); 
+}
