2ec18253df2898fbc16a0596a978dec6b9365bd9
Satrio Radithya Prayata-oss <prayata.satrio@gmail.com>
2026-06-09 13:19:50 +0700

    tambah kodingan input hadiah

--- main.c
+++ main.c
@@ -1,6 +1,9 @@
 #include "header_mesin.h"
+
 int main() {
     int pilihan;
+     Hadiah arrHadiah[100];
+     int jumlahHadiah = 0;
 
     do {
         // Menampilkan teks menu
@@ -10,21 +13,35 @@ int main() {
         printf("3. Simulasi Lite O\n");
         printf("4. Keluar\n");
         printf("Masukkan Menu: ");
-        
+
         scanf("%d", &pilihan);
 
         switch (pilihan) {
             case 1:
+            baca_hadiah(arrHadiah, &jumlahHadiah);
+        cetakTabelHadiah(arrHadiah, jumlahHadiah);
+                printf("\n--- TAMBAH HADIAH ---\n");
+                printf("Masukkan koordinat X: ");
+                scanf("%d", &x);
+                printf("Masukkan koordinat Y: ");
+                scanf("%d", &y);
+                printf("Masukkan Nama Hadiah : ");
+                scanf("%s", nama);
+                printf("Masukkan Skor: ");
+                scanf("%d", &skor);
+                tulisHadiah(arrHadiah, &jumlahHadiah, x, y, nama, skor);
+
+                printf("\n");
+                break;
+
+            case 2:
 
                 break;
-            case 2:
-            
-                break;
             case 3:
-            
+
                 break;
             case 4:
-    		printf("exit");
+                printf("exit");
                 break;
             default:
                 printf("Pilihan tidak valid. Silakan masukkan angka 1-4.\n");
@@ -34,4 +51,3 @@ int main() {
 
     return 0;
 }
-
