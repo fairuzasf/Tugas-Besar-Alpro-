7637ac9417ff66ab511d62520ed49483c8198e1d
fairuzasf <fayyyus97@gmail.com>
2026-06-10 17:34:46 +0700

    menambahkan library

--- header_mesin.h
+++ header_mesin.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <time.h>
 #include <string.h>
+#include <stdlib.h>
 
 typedef struct { 	//Pake typedef agar di main tinggal panggil namanya saja ex: Hadiah, ini biar tipe data beda tp bisa jd satu yaa
     int x;
