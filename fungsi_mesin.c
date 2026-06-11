857b84e732f73d20eb567c41aa2a5958ae5d62bd
Joannes4321 <2510514024@mahasiswa.upnvj.ac.id>
2026-06-11 14:33:00 +0700

    merapihkan tampilan posisi printf O.

--- fungsi_mesin.c
+++ fungsi_mesin.c
@@ -180,7 +180,7 @@ void tampilkanPapan(Hadiah arrHadiah[], int jumlahHadiah,
         for (j = 0; j < lebar; j++) {
             // ini buat cek O, klo dia sesuai sm kox & koy cetak
             if (j == kOx && i == kOy) {
-                printf("   O    ");
+                printf("    O    ");
             } else {
                 // kalo bukan o, hadiah yang di cek udah ada atau belum, kalo ada cetak nama & skornya
                 ketemu = 0;
